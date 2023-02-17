4969da4aa03d455c13e0a9c73589457485fe41ab
davey-mwaura <davidmburu50@gmail.com>
2023-02-18 01:50:32 +0300

    Initial commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,29 @@
-class Student 
+require_relative 'user'
 
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+
+  def learn(string)
+    @knowledge << string
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
+
+#Test student
+
+alice = Student.new("Alice", "Johnson")
+alice.learn("The capital of France is Paris")
+
+puts alice.knowledge
+# Output: ["The capital of France is Paris"]
+
+
